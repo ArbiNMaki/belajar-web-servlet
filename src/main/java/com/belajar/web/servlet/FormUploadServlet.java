062791a0107f784d41021f22504dd9a59a3a2907
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-29 22:25:14 +0700

    Upload File

--- src/main/java/com/belajar/web/servlet/FormUploadServlet.java
+++ src/main/java/com/belajar/web/servlet/FormUploadServlet.java
@@ -1,22 +1,28 @@
 package com.belajar.web.servlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
+import java.util.UUID;
 
-@WebServlet(urlPatterns = "/form")
-public class FormServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/form-upload")
+@MultipartConfig
+public class FormUploadServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        InputStream inputStream = FormServlet.class.getResourceAsStream("/html/form.html");
+        InputStream inputStream = FormUploadServlet.class.getResourceAsStream("/html/form-upload.html");
         if (inputStream == null) {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
@@ -30,10 +36,19 @@ public class FormServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String firstName = req.getParameter("firstName");
-        String lastName = req.getParameter("lastName");
-        String fullName = firstName + " " + lastName;
-        String response = "Hello " + fullName;
-        resp.getWriter().println(response);
+        String name = req.getParameter("name");
+        Part profile = req.getPart("profile");
+
+        // Ensure the upload directory exists
+        Path uploadDir = Path.of("upload");
+        if (!Files.exists(uploadDir)) {
+            Files.createDirectories(uploadDir);
+        }
+
+        Path uploadLocation = uploadDir.resolve(UUID.randomUUID().toString() + profile.getSubmittedFileName());
+        Files.copy(profile.getInputStream(), uploadLocation);
+
+        resp.getWriter().println("Hello " + name  + ", your profile saved in " + uploadLocation.toAbsolutePath());
     }
+
 }
